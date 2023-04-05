27903a4b2efca92bbdc7a88cf1e628c3a111aac4
Phuong <binkusip@gmail.com>
2023-04-05 23:25:43 +0700

    Spending Mana

---
+++ src/test/java/com/example/SpendingMana/SpendingManaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpendingMana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendingManaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
