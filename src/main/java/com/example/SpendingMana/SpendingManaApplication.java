a24a8e227f61f460d498c22b847348a92f60a4ba
khanhdeptraibodoi12 <dqk0910@gmail.com>
2023-04-08 22:38:00 +0700

    test github

--- src/main/java/com/example/SpendingMana/SpendingManaApplication.java
+++ src/main/java/com/example/SpendingMana/SpendingManaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpendingManaApplication {
-
+	// test
 	public static void main(String[] args) {
 		SpringApplication.run(SpendingManaApplication.class, args);
 	}
