7d689272db9a54945500e02b87e796340ed3d36d
khanhdeptraibodoi12 <dqk0910@gmail.com>
2023-04-09 17:54:27 +0700

    remove test

--- src/main/java/com/example/SpendingMana/SpendingManaApplication.java
+++ src/main/java/com/example/SpendingMana/SpendingManaApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpendingManaApplication {
-	// test
-	//test
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpendingManaApplication.class, args);
 	}
